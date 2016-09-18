27f73492ecdd72b12bcce177fe84ec1b68e744c1
隼人 <takebnn26@gmail.com>
2016-09-18 12:25:28 +0900

    復元

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,27 @@ class PrototypesController < ApplicationController
   def index
   end
   def new
+    @prototype = Prototype.new
+    @prototype.images.build
   end
+  def create
+    @prototype = current_user.prototypes.new(proto_params)
+    if @prototype.save
+      redirect_to root_path, notice: 'You succeeded in posting'
+    else
+      render :new, alert: 'You failed in posting'
+    end
+  end
+
   def show
+  end
+  private
+  def proto_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      images_attributes: [:id, :content,:content_type]
+    )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :email,:name, :group, :profile, :works, presence: true
   mount_uploader :image, ImageUploader
   has_many :prototypes
