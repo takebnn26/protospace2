47d56a18c2d10b1e2f492e95778dcdd7ff7130f7
隼人 <takebnn26@gmail.com>
2016-09-18 11:40:15 +0900

    image・prototype・userモデル作成

---
+++ db/migrate/20160918023649_add_ex_to_users.rb
@@ -0,0 +1,8 @@
+class AddExToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name,    :string
+    add_column :users, :group,   :string
+    add_column :users, :profile, :text
+    add_column :users, :works,   :string
+  end
+end
