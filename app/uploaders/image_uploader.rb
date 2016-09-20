00f1e9ced3740410be9e8c302699a9b850851c6b
隼人 <takebnn26@gmail.com>
2016-09-20 14:19:51 +0900

    復元作業 uploaderの設定変え new.html.haml修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,26 +4,8 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
 
-  process :resize_to_limit => [700, 700]
-
-  process :convert =>'jpg'
-
-  version :thumb do
-    process :resize_to_limit => [300,300]
-  end
-
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
-  def filename
-    supoer.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
-
-  def filename
-    time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-    name.downcase
+  def default_url
+    'sample.png'
   end
 
   storage :file
