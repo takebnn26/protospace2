d09e7c797de5b7b5da05f8f0b6468c97d70f6d4d
隼人 <takebnn26@gmail.com>
2016-09-21 17:31:32 +0900

    空のfields_forがnullで保存されてしまうエラー修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many   :images
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: proc { |attributes| attributes[:content].blank? }
+  validates :images, presence: true
 end
