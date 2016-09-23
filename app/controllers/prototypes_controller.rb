2e60c1722078b95246226ebb2f3331f878e22a4e
隼人 <takebnn26@gmail.com>
2016-09-24 00:14:10 +0900

    プロトタイプインデックス完成

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user).order("created_at DESC")
   end
   def new
     @prototype = Prototype.new
