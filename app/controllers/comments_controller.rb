fbf8c26680c81ba55d6e5e12682d6c9fae143e8a
隼人 <takebnn26@gmail.com>
2016-09-25 09:15:48 +0900

    コメント機能実装除外

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,2 @@
 class CommentsController < ApplicationController
-  # def create
-  #   @comment = current_user.comments.new(comment_params)
-  #   @comment.save
-  #   redirect_to controller: :prototypes, action: :show
-  # end
-  # private
-  # def comment_params
-  #   params.require(:comment).permit(
-  #     :text,
-  #     ).merge(
-  #     prototype_id: params[:prototype_id],
-  #     user_id: current_user.id
-  #     )
-  # end
 end
