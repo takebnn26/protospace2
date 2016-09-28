65b0324c9342b7d9b49d4512e7d3e076d685a598
隼人 <takebnn26@gmail.com>
2016-09-28 18:45:36 +0900

    destroyアクション実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
 
-  before_action :prototype_set, only: [:edit, :update, :show]
+  before_action :prototype_set, only: [:edit, :update, :show, :destroy]
   def index
     @prototypes = Prototype.includes(:user).order("created_at DESC")
   end
@@ -26,6 +26,13 @@ class PrototypesController < ApplicationController
       redirect_to root_path, notice: 'You suceeded in updating'
     else
       render :edit, alert: 'You failed in updating'
+    end
+  end
+  def destroy
+    if @prototype.destroy
+      redirect_to root_path, notice: 'You succeeded in deleting'
+    else
+      redirect_to root_path, alert: 'You failed in deleting'
     end
   end
   private
