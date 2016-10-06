bee5dcfaf302b2c0b2a5b783f9e82b9d8152a28d
隼人 <takebnn26@gmail.com>
2016-10-06 11:48:01 +0900

    like_user定義でいいねが既にしてあるかを調べる

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,4 +5,7 @@ class Prototype < ActiveRecord::Base
   has_many   :likes, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true, reject_if: proc { |attributes| attributes[:content].blank? }
   validates :images, :title, :concept, :catch_copy, presence: true
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
