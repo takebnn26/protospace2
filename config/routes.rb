3d285b77859e1f2375854d0fe94a9579e13c7989
隼人 <takebnn26@gmail.com>
2016-10-08 17:52:54 +0900

    like routing設定 resource prototypeにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes, except: :delete
+  resources :prototypes, except: :delete do
+    resources :likes, only: [:create, :destroy]
+  end
   # resources :comments, only: :create
   resources :users,      only: [:edit, :update, :show]
 end
