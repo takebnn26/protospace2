cb011522a536603f4b4fe0b32cca1c827c8e9d0f
隼人 <takebnn26@gmail.com>
2016-10-11 22:08:32 +0900

    tags_controllerの変数指定　tags/index・show作成　cssを当てる

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,12 @@
 class TagsController < ApplicationController
 
   def index
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = ActsAsTaggableOn::Tag.all.uniq
   end
 
   def show
-    @tag = ActsAsTaggableOn::Tag.find(params[:id])
-    @prototypes = Prototype.taggeed_with(@tag.name)
+    tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(tag.name)
   end
 
 end
